222e6e82f8f20bcb1890066d770751f4a5a2348a
Alisha Khan <Alishakhanbhalesultan@gamil.com>
2026-09-09 20:09:26 +0530

    chnagrd

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 name = "johen deo "
 id = 12345678910
+mother_name ="shamima"
 standard = "10th Grade"
 print(f"Student Name: {name}")
 print(f"Student ID: {id}")
 print(f"Student Standard: {standard}")
+print(f"Mother's Name: {mother_name}")
